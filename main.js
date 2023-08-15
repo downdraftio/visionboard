0a7a3e86c3154846845d1503c43e584c28a47361
Downdraft <122659847+downdraftio@users.noreply.github.com>
2023-08-15 18:41:05 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preinit() { 
+    let access = sessionStorage.getItem("token");
+    if (access = qeklrj23klj2389s) {
+        init(); 
+    }
+}
+
+function init() {
+    console.log("200"); 
+}
+
+
+preinit(); 
